0e2ab588a9d21bd4273175095710bec7d38514ba
AmirMohammad <amir.b@sindibad.iq>
2026-04-15 13:50:12 +0330

    add Google Tag Manager

--- apps/villa-sites/app/layout.tsx
+++ apps/villa-sites/app/layout.tsx
@@ -83,6 +83,17 @@ export default async function RootLayout({
       className={`${fontVars} ${themeClass}`.trim()}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NBNBWQ88');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* GSC verification */}
         {ctx?.site.gscVerificationToken && (
           <meta
@@ -96,6 +107,16 @@ export default async function RootLayout({
         )}
       </head>
       <body className="min-h-dvh flex flex-col font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NBNBWQ88"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Suspense fallback={null}>
           <VillaHtmlDirSync pattern="path-lang" />
         </Suspense>
